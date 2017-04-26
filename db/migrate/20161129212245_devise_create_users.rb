f71d8afcc750007c32465d3edb07c11538ce1161
RJ Robinson <robert_robinson@cable.comcast.com>
2017-04-25 18:29:57 -0700

    generated devise views

--- db/migrate/20161129212245_devise_create_users.rb
+++ db/migrate/20161129212245_devise_create_users.rb
@@ -14,8 +14,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
+
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
+
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
 
