7513f6b16c63e09619b8f80e123f6ff9888be985
RJ Robinson <robert_robinson@cable.comcast.com>
2017-04-26 11:38:50 -0700

    altered navigation to automotically be minimized

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,18 +1,23 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  # ==> Mailer Configuration
-  # Configure the e-mail address which will be shown in Devise::Mailer,
-  # note that it will be overwritten if you use your own mailer class
-  # with default "from" parameter.
+
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+
   require 'devise/orm/active_record'
 
   config.sign_out_via = :get
+
   config.stretches = Rails.env.test? ? 1 : 11
+
   config.reconfirmable = true
+
   config.expire_all_remember_me_on_sign_out = true
+
   config.password_length = 6..128
+
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+
   config.reset_password_within = 6.hours
+
 end
