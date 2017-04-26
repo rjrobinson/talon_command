2d5912560ea1a266132c3961bef345be66e9625e
RJ Robinson <robert_robinson@cable.comcast.com>
2017-04-26 07:11:23 -0700

    modified the JS file, and added a first and last name to the user model. also added a better sign up page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require metisMenu/jquery.metisMenu.js
 //= require pace/pace.min.js
 //= require slimscroll/jquery.slimscroll.min.js
+//= require toastr/toastr.min.js
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,20 @@
 module ApplicationHelper
-    def is_active_controller(controller_name)
-        params[:controller] == controller_name ? "active" : nil
-    end
+  def is_active_controller(controller_name)
+    params[:controller] == controller_name ? "active" : nil
+  end
 
-    def is_active_action(action_name)
-        params[:action] == action_name ? "active" : nil
+  def is_active_action(action_name)
+    params[:action] == action_name ? "active" : nil
+  end
+
+  def flash_messages(opts = {})
+    flash_messages = []
+    flash.each do |type, message|
+      type = 'success' if type == 'notice'
+      type = 'error' if type == 'alert'
+      text = "<script>toastr.#{type}('#{message}');</script>"
+      flash_messages << text.html_safe if message
     end
+    flash_messages.join("\n").html_safe
+  end
 end
